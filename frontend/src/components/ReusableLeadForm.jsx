98b19c90e1a7c6429606106dc2a8248ccc654261
BlueBuck <gauravquest00@gmail.com>
2026-05-18 13:28:18 +0530

    Update ReusableLeadForm.jsx

--- frontend/src/components/ReusableLeadForm.jsx
+++ frontend/src/components/ReusableLeadForm.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import '../style/Reusable.css'; // Import CSS
 
-const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE = import.meta.env.VITE_API_URL || 'https://luxurynest.onrender.com/api';
 
 function ReusableLeadForm({
   isOpen,
